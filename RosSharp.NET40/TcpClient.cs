1236bd48c53695559f58e304aa489cf5b9cc95b0
zoetrope <a.ikezoe@gmail.com>
2012-02-05 23:22:18 +0900

    add TcpClient

---
+++ RosSharp.NET40/TcpClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RosSharp
+{
+    class TcpClient
+    {
+        private Socket _socket;
+        
+        public void Connect(string hostName, int portNumber)
+        {
+            var hostEntry = new DnsEndPoint(hostName, portNumber);
+
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var socketEventArg = new SocketAsyncEventArgs {RemoteEndPoint = hostEntry};
+
+            Observable.FromEventPattern<SocketAsyncEventArgs>(
+                e => socketEventArg.Completed += e,
+                e => socketEventArg.Completed -= e);
+
+            _socket.ConnectAsync(socketEventArg);
+
+        }
+    }
+}
--- Sample/Talker/Program.cs
+++ Sample/Talker/Program.cs
@@ -23,12 +23,12 @@ namespace Talker
 
             //var ret = client.RegisterServiceAsync("/talker", "myservice", new Uri("http://192.168.11.2:11112"), new Uri("http://192.168.11.2:11111")).First();
             //client.UnregisterServiceAsync("/talker", null, new Uri("http://localhost")).First();
-            
-            //var ret1 = client.RegisterSubscriberAsync("/test", "topic1", "std_msgs/String", new Uri("http://192.168.11.2:11112")).First();
-            //var ret2 = client.RegisterPublisherAsync("/test", "topic1", "std_msgs/String", new Uri("http://192.168.11.2:11112")).First();
-            var ret2 = client.RegisterPublisherAsync("/test", "chatter", "std_msgs/String", new Uri("http://192.168.11.2:11112")).First();
 
-            var slave = new SlaveClient(ret2.First());
+            var ret1 = client.RegisterSubscriberAsync("/test", "chatter", "std_msgs/String", new Uri("http://192.168.11.2:11112")).First();
+            //var ret2 = client.RegisterPublisherAsync("/test", "topic1", "std_msgs/String", new Uri("http://192.168.11.2:11112")).First();
+            //var ret2 = client.RegisterPublisherAsync("/test", "chatter", "std_msgs/String", new Uri("http://192.168.11.2:11112")).First();
+
+            var slave = new SlaveClient(ret1.First());
 
             //var pid = slave.GetPidAsync("/test").First();
             //var businfo = slave.GetBusInfoAsync("/test").First();
