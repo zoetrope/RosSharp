697bee71c16745f768c8db9ac0076d4247297915
zoetrope <a.ikezoe@gmail.com>
2012-02-05 12:06:55 +0900

    add proxy

---
+++ RosSharp.NET40/ParameterServerProxy.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Reflection;
+using CookComputing.XmlRpc;
+
+namespace RosSharp
+{
+    [XmlRpcUrl("")]
+    public class ParameterServerProxy : XmlRpcClientProtocol
+    {
+        [XmlRpcBegin("deleteParam")]
+        public IAsyncResult BeginDeleteParam(string callerId, string key, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId, key }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndDeleteParam(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+
+        [XmlRpcBegin("setParam")]
+        public IAsyncResult BeginSetParam(string callerId, string key, object value, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId, key, value }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndSetParam(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+
+        [XmlRpcBegin("getParam")]
+        public IAsyncResult BeginGetParam(string callerId, string key, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId, key }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndGetParam(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+
+        [XmlRpcBegin("searchParam")]
+        public IAsyncResult BeginSearchParam(string callerId, string key, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId, key }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndSearchParam(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+
+        [XmlRpcBegin("subscribeParam")]
+        public IAsyncResult BeginSubscribeParam(string callerId, string key, string callerApi, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId, key, callerApi }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndSubscribeParam(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+
+        [XmlRpcBegin("unsubscribeParam")]
+        public IAsyncResult BeginUnsubscribeParam(string callerId, string key, string callerApi, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId, key, callerApi }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndUnsubscribeParam(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+
+        [XmlRpcBegin("hasParam")]
+        public IAsyncResult BeginHasParam(string callerId, string key, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId, key }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndHasParam(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+
+        [XmlRpcBegin("getParamNames")]
+        public IAsyncResult BeginGetParamNames(string callerId, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndGetParamNames(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+    }
+}
---
+++ RosSharp.NET40/SlaveProxy.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Reflection;
+using CookComputing.XmlRpc;
+
+namespace RosSharp
+{
+    [XmlRpcUrl("")]
+    public class SlaveProxy : XmlRpcClientProtocol
+    {
+        [XmlRpcBegin("getBusStats")]
+        public IAsyncResult BeginGetBusStats(string callerId, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndGetBusStats(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+
+        [XmlRpcBegin("getBusInfo")]
+        public IAsyncResult BeginGetBusInfo(string callerId, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndGetBusInfo(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+        
+        [XmlRpcBegin("getMasterUri")]
+        public IAsyncResult BeginGetMasterUri(string callerId, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndGetMasterUri(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+        
+        [XmlRpcBegin("shutdown")]
+        public IAsyncResult BeginShutdown(string callerId, string msg, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId, msg }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndShutdown(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+        
+        [XmlRpcBegin("getPid")]
+        public IAsyncResult BeginGetPid(string callerId, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndGetPid(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+        
+        [XmlRpcBegin("getSubscriptions")]
+        public IAsyncResult BeginGetSubscriptions(string callerId, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndGetSubscriptions(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+        
+        [XmlRpcBegin("getPublications")]
+        public IAsyncResult BeginGetPublications(string callerId, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndGetPublications(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+        
+        [XmlRpcBegin("paramUpdate")]
+        public IAsyncResult BeginParamUpdate(string callerId, string parameterKey, object parameterValue, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId, parameterKey, parameterValue }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndParamUpdate(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+        
+        [XmlRpcBegin("publisherUpdate")]
+        public IAsyncResult BeginPublisherUpdate(string callerId, string topic, string[] publishers, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId, topic, publishers }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndPublisherUpdate(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+        
+        [XmlRpcBegin("requestTopic")]
+        public IAsyncResult BeginRequestTopic(string callerId, string topic, object[] protocols, AsyncCallback callback, object state)
+        {
+            return BeginInvoke(MethodBase.GetCurrentMethod(), new object[] { callerId, topic, protocols }, callback, state);
+        }
+        [XmlRpcEnd]
+        public object[] EndRequestTopic(IAsyncResult result)
+        {
+            return (object[])EndInvoke(result);
+        }
+    }
+}
