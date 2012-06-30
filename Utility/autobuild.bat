23847b73fd9d95037635d6c260c003f910249e36
zoetrope <a.ikezoe@gmail.com>
2012-06-30 11:48:27 +0900

    prepare release

---
+++ Utility/autobuild.bat
@@ -0,0 +1,66 @@
+@Set Path=C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319;%PATH%
+rem msbuild ..\RosSharp\RosSharp.csproj /p:Configuration=Release /t:Rebuild
+rem msbuild ..\App\GenMsg.Lib\GenMsg.Lib.fsproj /p:Configuration=Release /t:Rebuild
+rem msbuild ..\App\GenMsg\GenMsg.csproj /p:Configuration=Release /t:Rebuild
+rem msbuild ..\App\RosCore\RosCore.csproj /p:Configuration=Release /t:Rebuild
+
+set RosSharpDir=.\RosSharp-0.1.0
+
+rem mkdir %RosSharpDir%
+rem 
+rem robocopy ..\ .\%RosSharpDir%\ License.txt
+rem 
+rem mkdir %RosSharpDir%\bin
+rem 
+rem robocopy ..\App\RosCore\bin\Release\ .\%RosSharpDir%\bin *.dll
+rem robocopy ..\App\RosCore\bin\Release\ .\%RosSharpDir%\bin *.xml
+rem robocopy ..\App\RosCore\bin\Release\ .\%RosSharpDir%\bin RosCore.exe
+rem robocopy ..\App\RosCore\bin\Release\ .\%RosSharpDir%\bin RosCore.exe.config
+rem 
+rem robocopy ..\App\GenMsg\bin\Release\ .\%RosSharpDir%\bin *.dll
+rem robocopy ..\App\GenMsg\bin\Release\ .\%RosSharpDir%\bin *.xml
+rem robocopy ..\App\GenMsg\bin\Release\ .\%RosSharpDir%\bin GenMsg.exe
+rem 
+rem mkdir %RosSharpDir%\doc
+rem 
+rem robocopy ..\Doc\build\html .\%RosSharpDir%\doc
+
+mkdir %RosSharpDir%\sample
+
+mkdir %RosSharpDir%\sample\AddTwoIntsClient
+
+robocopy ..\Sample\AddTwoIntsClient .\%RosSharpDir%\sample\AddTwoIntsClient Program.cs
+robocopy ..\Sample\AddTwoIntsClient .\%RosSharpDir%\sample\AddTwoIntsClient AddTwoIntsClient.csproj
+robocopy ..\Sample\AddTwoIntsClient .\%RosSharpDir%\sample\AddTwoIntsClient app.config
+robocopy ..\Sample\AddTwoIntsClient\Properties .\%RosSharpDir%\sample\AddTwoIntsClient\Properties
+
+mkdir %RosSharpDir%\sample\AddTwoIntsServer
+
+robocopy ..\Sample\AddTwoIntsServer .\%RosSharpDir%\sample\AddTwoIntsServer Program.cs
+robocopy ..\Sample\AddTwoIntsServer .\%RosSharpDir%\sample\AddTwoIntsServer AddTwoIntsServer.csproj
+robocopy ..\Sample\AddTwoIntsServer .\%RosSharpDir%\sample\AddTwoIntsServer app.config
+robocopy ..\Sample\AddTwoIntsServer\Properties .\%RosSharpDir%\sample\AddTwoIntsServer\Properties
+
+mkdir %RosSharpDir%\sample\Listener
+
+robocopy ..\Sample\Listener .\%RosSharpDir%\sample\Listener Program.cs
+robocopy ..\Sample\Listener .\%RosSharpDir%\sample\Listener Listener.csproj
+robocopy ..\Sample\Listener .\%RosSharpDir%\sample\Listener app.config
+robocopy ..\Sample\Listener\Properties .\%RosSharpDir%\sample\Listener\Properties
+
+mkdir %RosSharpDir%\sample\ParameterClient
+
+robocopy ..\Sample\ParameterClient .\%RosSharpDir%\sample\ParameterClient Program.cs
+robocopy ..\Sample\ParameterClient .\%RosSharpDir%\sample\ParameterClient ParameterClient.csproj
+robocopy ..\Sample\ParameterClient .\%RosSharpDir%\sample\ParameterClient app.config
+robocopy ..\Sample\ParameterClient\Properties .\%RosSharpDir%\sample\ParameterClient\Properties
+
+
+mkdir %RosSharpDir%\sample\Talker
+
+robocopy ..\Sample\Talker .\%RosSharpDir%\sample\Talker Program.cs
+robocopy ..\Sample\Talker .\%RosSharpDir%\sample\Talker Talker.csproj
+robocopy ..\Sample\Talker .\%RosSharpDir%\sample\Talker app.config
+robocopy ..\Sample\Talker\Properties .\%RosSharpDir%\sample\Talker\Properties
+
+pause
